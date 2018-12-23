d01fcfe87387520f38e39d99bcac11cfce2759de
Giorgi Modebadze <giorgi9449@gmail.com>
2018-12-23 11:30:31 +0100

    Result

--- R/myhome.R
+++ R/myhome.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(readr)
 library(stringr)
-
+rm(list= ls())
 data = read.delim("/Users/Raviky/OneDrive - hu-berlin.de/Programming/Python/WebScaping/MYhome/myHome.txt", 
                   sep="~", header = F, na.strings = "NA",strip.white = T, stringsAsFactors = F,
                   quote = "")
@@ -27,6 +27,11 @@ word(data$address, -1)
 data = mutate(data, city = word(address, -1))
 data = mutate(data, subCity = word(address, -2))
 
+data$subCity = gsub(pattern = ",", replacement = "", x = data$subCity)
+data$subCity = tolower(data$subCity)
+data$subCity  = trimws(data$subCity, "both")
+data$subCity = ifelse(data$subCity == "(თემქა)", "temka", data$subCity)
+
 # data %>% group_by(type) %>% summarise(avgPrice = mean(price), total = n()) %>% 
 #   arrange(desc(total))
 # 
@@ -34,7 +39,51 @@ data = mutate(data, subCity = word(address, -2))
 #   ggplot(aes(y=price)) + geom_boxplot() + 
 #   facet_wrap(~ type)+ scale_y_log10() 
 
-count(data, city) %>% arrange(desc(n))
+count(data, city, subCity) %>% arrange(desc(n))
 count(data, type) %>% arrange(n)
 count(data, type) %>% arrange(desc(n))
 
+#take a look at tbilisi
+head(data)
+
+filter(data, city == "Tbilisi") %>% count(subCity, type) %>% arrange((n)) %>% 
+  filter(n > 100) %>% ggplot(aes(x= subCity, y = n)) + geom_col() + coord_flip() +
+  facet_wrap(~ type)
+
+filter(data, city == "Tbilisi") %>% count(type)%>% arrange(desc(n)) %>% filter(n > 100)
+# my scope
+list = c("Newly finished apartment for sale",
+         "Older finished apartment for sale")
+
+list2RentData = c("Newly finished apartment for rent",
+                  "Older finished apartment for rent")
+
+# TbilisiSale
+dataAboutApartmentsForSale = filter(data, type %in% list, city =="Tbilisi")
+
+dataAboutApartmentsForSale %>% filter(price > 10000, price < 3000000) %>%
+  ggplot(aes(x= type, y = price))  + geom_boxplot() + scale_y_log10() + facet_wrap(~subCity)
+
+dataAboutApartmentsForSale[is.na(dataAboutApartmentsForSale)] = 0
+dataAboutApartmentsForSale = dataAboutApartmentsForSale %>% filter(price > 10000, price < 3000000, area < 900, rooms < 20, rooms >0,
+                                                                   floor < 30, bedRooms < 15)
+output1 = select(dataAboutApartmentsForSale, type, subCity,price,bedRooms,rooms,floor,area) %>%
+  mutate(stype = "sale",type =ifelse(type =="Newly finished apartment for sale","New","Old"))
+
+# TbilisiRent
+dataAboutApartmentsForRent = filter(data, type %in% list2RentData, city =="Tbilisi")
+dataAboutApartmentsForRent$price = ifelse(is.na(dataAboutApartmentsForRent$price),0,dataAboutApartmentsForRent$price)
+
+dataAboutApartmentsForRent[is.na(dataAboutApartmentsForRent)] = 0
+
+dataAboutApartmentsForRent %>% filter(price > 200, price < 20000) %>%
+  ggplot(aes(x= type, y = price))  + geom_boxplot() + scale_y_log10() + facet_wrap(~subCity)
+
+dataAboutApartmentsForRent = dataAboutApartmentsForRent %>% filter(price > 200, price < 20000, area < 500, rooms < 10, rooms >0,
+                                      floor < 30, bedRooms < 10)
+
+output2 = select(dataAboutApartmentsForRent, type, subCity,price,bedRooms,rooms,floor,area) %>%
+  mutate(stype = "rent",type =ifelse(type =="Newly finished apartment for rent","New","Old"))
+
+
+write.csv(union_all(output1,output2), "~/Desktop/myData.csv",quote = F,row.names = F)
